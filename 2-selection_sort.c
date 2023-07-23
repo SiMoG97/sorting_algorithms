f154cb5c468cc37a9d6d72fed47ca665f5564e83
SiMoG97 <echaaranimohamed@gmail.com>
2023-07-22 17:53:21 -0700

    Task-2: adding 2-O and 2-selection_sort.c files

---
+++ 2-selection_sort.c
@@ -0,0 +1,41 @@
+#include "sort.h"
+
+/**
+ * selection_sort - sorts an array of integers in ascending order
+ *                  using the Selection sort algorithm
+ * @array: the array to sort
+ * @size: the size of the array
+ * Return: Nothing
+ */
+
+void selection_sort(int *array, size_t size)
+{
+	int temp;
+	size_t i, j, min;
+	bool didSwap = false;
+
+	if (!array || size < 2)
+		return;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		min = i;
+		didSwap = false;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min])
+			{
+				min = j;
+				didSwap = true;
+			}
+		}
+
+		if (!didSwap)
+			continue;
+
+		temp = array[i];
+		array[i] = array[min];
+		array[min] = temp;
+		print_array(array, size);
+	}
+}
